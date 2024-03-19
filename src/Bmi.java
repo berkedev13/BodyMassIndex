59b37ae0128462e51ac4525ef584079a823eab27
Berke <berkesat.dev@gmail.com>
2024-03-20 00:20:09 +0300

    First Commit

--- src/Bmi.java
+++ src/Bmi.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Bmi {
+import java.util.Scanner;
+public class Bmi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your height: ");
+        double height = input.nextDouble();
+        System.out.print("Please enter your mass: ");
+        double mass = input.nextDouble();
+        System.out.println("Your body mass index is " + (mass / (height * height)));
+    }
 }
